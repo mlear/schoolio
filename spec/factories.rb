93af11739279cfabd0af21c0eb0a34ef0771f6fa
Martin Lear & Zach Pflederer <pair+mlear.zpfled@devbootcamp.com>
2014-07-17 08:47:41 -0500

    refactor features/user_spec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,11 @@
 FactoryGirl.define do
+  factory :unsaved_user, class: User do
+    first_name "Leslie"
+    last_name "Knope"
+    email "waffles@whipped_cream.yum"
+    password "foobar"
+  end
+
   factory :valid_user, class: User do
     first_name "Ron"
     last_name "Swanson"
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 system 'clear'
 
 describe 'User pages' do
-  let(:user) { User.last }
-  context 'on the homepage' do
+  let(:user) { create :valid_user }
+  let(:new_user) { build :unsaved_user }
 
+  context 'on the homepage' do
     before { visit root_path }
     subject { page }
 
@@ -13,69 +14,49 @@ describe 'User pages' do
     it { should have_content 'Sign Up'}
 
     describe 'when a user signs up' do
-
       context 'with valid information' do
-        let(:first_name)  { "Ron" }
-        let(:last_name)  { "Swanson" }
-        let(:new_email) { "new@example.com" }
-        let(:password) { "foo" }
-
         it 'should redirect to user profile' do
-          fill_in "First name",             with: first_name
-          fill_in "Last name",              with: last_name
-          fill_in "Email",                  with: new_email
-          fill_in "Password",               with: password
-          fill_in "Password confirmation",  with: password
+          fill_in "First name",             with: new_user.first_name
+          fill_in "Last name",              with: new_user.last_name
+          fill_in "Email",                  with: new_user.email
+          fill_in "Password",               with: new_user.password
+          fill_in "Password confirmation",  with: new_user.password
           click_button "sign_up"
-          expect(current_path).to eq(user_path(user))
+          expect(current_path).to eq user_path User.last
         end
       end
-      context 'with invalid information' do
-        let(:first_name)  { "Ron" }
-        let(:last_name)  { "Swanson" }
-        let(:new_email) { "new@example.com" }
-        let(:password) { "foo" }
 
+      context 'with invalid information' do
         it 'should redirect to root' do
-          User.create(first_name: "Ron", last_name: "Swanson", email: "new@example.com", password: "foo")
-          fill_in "First name",             with: first_name
-          fill_in "Last name",              with: last_name
-          fill_in "Email",                  with: new_email
-          fill_in "Password",               with: password
-          fill_in "Password confirmation",  with: password
+          fill_in "First name",             with: new_user.first_name
+          fill_in "Last name",              with: new_user.last_name
+          fill_in "Email",                  with: user.email
+          fill_in "Password",               with: new_user.password
+          fill_in "Password confirmation",  with: new_user.password
           click_button "sign_up"
           expect(current_path).to eq(root_path)
         end
-
       end
     end
 
-    describe 'when a user signs in' do
-      before { visit root_path }
-      subject { page }
-
-      describe 'with valid information' do
+    describe 'when a user logs in' do
+      context 'with valid information' do
         it 'logs in a user and redirects to user profile' do
-          user = create :valid_user
-          fill_in "Login email",     with: "new@example.com"
-          fill_in "Login password",  with: "foo"
+          fill_in "Login email",     with: user.email
+          fill_in "Login password",  with: user.password
           click_button "sign_in"
-          expect(current_path).to eq(user_path(user))
+          expect(current_path).to eq user_path user
         end
       end
 
       describe 'with invalid information' do
         it 'redirects to home page' do
-          fill_in "Login email",     with: "newbie@example.com"
-          fill_in "Login password",  with: "foo"
+          fill_in "Login email",     with: user.email
+          fill_in "Login password",  with: 'stupid-head'
           click_button "sign_in"
           expect(current_path).to eq(root_path)
         end
-
       end
-
     end
-
   end
-
 end
