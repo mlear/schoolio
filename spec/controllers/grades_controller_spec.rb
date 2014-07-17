d30e351d3517b1226f06c76c78c0622871f1b8b7
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-17 08:13:41 -0500

    fixed controller tests

--- spec/controllers/grades_controller_spec.rb
+++ spec/controllers/grades_controller_spec.rb
@@ -2,43 +2,28 @@ require 'rails_helper'
 
 RSpec.describe GradesController, :type => :controller do
 
-  # describe "GET 'index'" do
-  #   it "returns http success" do
-  #     # get 'index'
-  #     visit grades_url 
-  #     expect(current_path.to_s).to eq('/grades')
-  #   end
-  # end
+  let(:grade) { Grade.create(user_id: 1, course_id: 1, gpa: 2.0) }
 
-  # describe "GET 'create'" do
-  #   it "returns http success" do
-  #     get 'create'
-  #     expect(response).to be_success
-  #   end
-  # end
+  describe "GET 'index'" do
+    it "returns http success" do
+      # get 'index'
+      visit grades_url 
+      expect(current_path.to_s).to eq('/grades')
+    end
+  end
 
-  # describe "GET 'delete'" do
-  #   it "returns http success" do
-  #     get 'delete'
-  #     expect(response).to be_success
-  #   end
-  # end
+  describe "GET 'show'" do
+    it "returns http success" do
+      grade_url(grade) 
+      expect(response).to be_success
+    end
+  end
 
-  # describe "GET 'show'" do
-  #   it "returns http success" do
-  #     get 'show'
-  #     expect(response).to be_success
-  #   end
-  # end
-
-  # describe "GET 'new'" do
-  #   it "returns http success" do
-  #     # get 'new'
-  #     # expect(response).to be_success
-  #     # visit '/grades/new' 
-  #     visit grades_url
-  #     expect(current_path.to_s).to eq('/grades/new')
-  #   end
-  # end
+  describe "GET 'new'" do
+    it "returns http success" do
+      visit new_grade_url
+      expect(current_path.to_s).to eq('/grades/new')
+    end
+  end
 
 end
