da69431540753b40648e1028bd81ef5d0ebd9a20
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-18 15:52:49 -0500

    merging with dv

--- db/schema.rb
+++ db/schema.rb
@@ -86,7 +86,6 @@ ActiveRecord::Schema.define(version: 20140718184145) do
     t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
-    t.string   "role_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
