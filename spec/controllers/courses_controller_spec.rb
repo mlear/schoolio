3c6e0adb02f18efe4636f4ee7481d5e24bc7eb47
Joey Rosztoczy <joeyr@stotzeq.com>
2014-07-17 19:20:39 -0500

    coverin dat controller hashtag update

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe GradesController, :type => :controller do
+RSpec.describe CoursesController, :type => :controller do
 
   let(:course) { Course.create(subject: "Calculus", name: "Calc 101a") }
 
@@ -23,6 +23,21 @@ RSpec.describe GradesController, :type => :controller do
     it "returns http success" do
       visit new_course_url
       expect(current_path).to eq('/courses/new')
+    end
+  end
+
+  describe '#update' do
+
+    let(:course1) {{id: course.id, course: { subject: "Biology" }}}
+
+
+    context 'with form fill out' do
+
+      it 'updates course information' do
+        post :update, course1
+        expect(assigns(:course).subject).to eq 'Biology'
+        expect(response.redirect?).to be true
+      end
     end
   end
 
